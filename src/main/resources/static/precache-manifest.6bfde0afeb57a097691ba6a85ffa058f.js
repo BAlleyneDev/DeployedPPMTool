f0839adb7fa05049633bf6fac5631bf6d3b00b9a
BAlleyneUWO <balleyn@uwo.ca>
2020-04-01 14:37:29 -0400

    React

---
+++ src/main/resources/static/precache-manifest.6bfde0afeb57a097691ba6a85ffa058f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c909b16958940de0d21df591af065ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3aed528757f232343d3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0d9d0973a0e4d7dda964",
+    "url": "/static/css/main.b3b1f41a.chunk.css"
+  },
+  {
+    "revision": "d3aed528757f232343d3",
+    "url": "/static/js/2.e7e53480.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e7e53480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d9d0973a0e4d7dda964",
+    "url": "/static/js/main.66c5de23.chunk.js"
+  },
+  {
+    "revision": "165f034ffb316832c97f",
+    "url": "/static/js/runtime-main.86e74220.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
